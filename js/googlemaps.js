aefb265a727172b1c82965e59a9e00058e145c41
andreamogren <andrea.mogren91@gmail.com>
2017-02-02 20:14:40 +0100

    Made API work, centered gallery, added about and favicon

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,19 +1,19 @@
 var mapOptions = {
-    center: new google.maps.LatLng(37.7831, -122.4039),
+    center: new google.maps.LatLng(59.337238,18.0676545),
     zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
 };
-var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
-/*var markerOptions = {
-    position: new google.maps.LatLng(37.7831, -122.4039),
+var markerOptions = {
+    position: new google.maps.LatLng(59.337238,18.0676545),
     map: map
 };
 var marker = new google.maps.Marker(markerOptions);
 marker.setMap(map);
 
 var infoWindowOptions = {
-    content: 'Off the Grid'
+    content: 'Off the Grid, David Bagares gata 19 Stockholm'
 };
 
 var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
@@ -21,4 +21,4 @@ google.maps.event.addListener(marker,'click',function(e){
 
   infoWindow.open(map, marker);
 
-});*/
+});
